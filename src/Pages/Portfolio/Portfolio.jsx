5c7c1a31ef0ad484583c3d6772a077383006f3ad
paixtechdom <paixtechdom@gmail.com>
2024-07-09 16:54:09 +0100

    updated Portfolio.jsx

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -128,7 +128,7 @@ const APortfolio = ({portfolio, i}) => {
 
         <div className="flex flex-col gap-5 absolute bottom-0 min-h-[10vh] w-full text-center">
             <Parallax id={portfolio.title.replaceAll(' ', '')+'header'}>
-                <a target="_blank" href={portfolio.link} className="text-center border border-blue-900 bg-primary bg-opacity-80 p-3 px-6 rounded-full shadow-2xl flex flex-wrap items-center gap-2 justify-center"><span>Click to view</span> <strong className="text-white"> {portfolio.title}</strong></a>
+                <a target="_blank" href={portfolio.link} className="text-center border border-blue-900 bg-primary bg-opacity-80 p-3 px-6 rounded-full shadow-2xl flex flex-wrap items-center gap-2 justify-center max-w-[90%]"><span>Click to view</span> <strong className="text-white"> {portfolio.title}</strong></a>
             </Parallax>
            
         </div>
